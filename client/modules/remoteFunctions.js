91c5b2e8d4c991a0ef00089a82e89308f6ccf5e2
Djeeberjr <niklaskapelle@gmail.com>
2019-06-13 14:24:13 +0200

    added remote fnc for interactive console to client

--- client/modules/remoteFunctions.js
+++ client/modules/remoteFunctions.js
@@ -1,6 +1,14 @@
 const remoteFunctions = {
 	pong(){
 		mp.gui.chat.push("Pong from chat push");
+	},
+	interactive(code){
+		if(!DEVELOPMENT) return;
+		mp.gui.chat.push("cunt");
+		(()=>{
+			"use strict";
+			let rtn = eval(code);
+		})();
 	}
 };
 
